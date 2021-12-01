b2356a69f8df1bce3b810cf78b785f89b0ec3a0b
Djina <djina.djoric98@gmail.com>
2021-12-01 22:38:10 +0100

    initial commit

---
+++ src/project/handler.clj
@@ -0,0 +1,11 @@
+(ns project.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "<h1>Bee organic</h1>")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
