d6aa7525e6633fd8e10314a6cd17435c92c7658c
Djina <djina.djoric98@gmail.com>
2021-12-07 10:39:04 +0100

    css styling & admin login

--- src/project/handler.clj
+++ src/project/handler.clj
@@ -3,24 +3,54 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :as response]
+            [ring.middleware.session :as session]
             [project.db :as db]
-            [project.pages :as pages]))
+            [project.pages :as pages]
+            [project.admin :as admin]))
 
 (defroutes app-routes
-  (GET "/" [] (pages/index (db/get-producers)))
+           (GET "/" [] (pages/index (db/get-producers)))
+
+           (GET "/producers/:producer-id" [producer-id] (pages/producer (db/get-producer-by-id producer-id)))
+
+           (GET "/admin/login" [:as {session :session}]
+             (if (:admin session)
+               (response/redirect "/")
+               (pages/admin-login)))
+
+           (POST "/admin/login" [username password]
+             (if(admin/authorize-admin username password)
+               (-> (response/redirect "/")
+                   (assoc-in [:session :admin] true))
+               (pages/admin-login "Pogrešna email adresa ili lozinka!")))
+
+           (GET "/admin/logout" []
+             (-> (response/redirect "/")
+                 (assoc-in [:session :admin] false)))
+
+           (route/not-found "Not Found"))
+
+(defroutes admin-routes
            (GET "/producers/new" [] (pages/edit-producer nil))
+
            (POST "/producers" [name address contact description]
              (do (db/create-producer name address contact description)
                  (response/redirect "/")))
 
            (GET "/producers/:producer-id/edit" [producer-id] (pages/edit-producer (db/get-producer-by-id producer-id)))
+
            (POST "/producers/:producer-id" [producer-id name address contact description]
              (do (db/update-producer producer-id name address contact description)
-                 (response/redirect (str "/producers/" producer-id))))
+                 (response/redirect (str "/producers/" producer-id)))))
 
-           (GET "/producers/:producer-id" [producer-id] (pages/producer (db/get-producer-by-id producer-id)))
-           (route/not-found "Not Found"))
-
+(defn wrap-admin-only [handler]
+  (fn [request]
+    (if (-> request :session :admin)
+      (handler request)
+      (response/redirect "/admin/login"))))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (routes (wrap-routes admin-routes wrap-admin-only)
+              app-routes)
+      (wrap-defaults site-defaults)
+      session/wrap-session))
--- src/project/pages.clj
+++ src/project/pages.clj
@@ -5,17 +5,37 @@
 
 (defn template [& body]
   (html5
-    [:head [:title "Bee organic"]]
+    [:head [:title "Bee organic"]
+     [:link {:rel "stylesheet"
+             :href "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+             :integrity "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+             :crossorigin "anonymous"}]]
     [:body
-     [:a {:href "/"} [:h1 "Bee organic"]]
-     [:a {:href "producers/new"} "Dodaj novog proizvođača"]
-     [:hr]
-     body]))
+     [:div.container
+      [:nav.navbar.navbar-expand-lg.navbar-light.bd-light
+       [:a.navbar-brand {:href "/"} "Bee organic"]
+       [:div.navbar-nav.ml-auto
+        [:a.nav-item.nav-link {:href "/producers/new"} "Dodaj novog proizvođača"]
+        [:a.nav-item.nav-link {:href "/admin/login"} "Uloguj se"]
+        [:a.nav-item.nav-link {:href "/admin/logout"} "Odjavi se"]
+        ]]
+      body]]))
+
+(def preview-length 270)
+
+(defn- cut-description [description]
+  (if (> (.length description) preview-length)
+    (str (subs description 0 preview-length) "...")
+    description))
 
 (defn index [producers]
   (template
     (for [p producers]
-      [:h2 [:a {:href (str "/producers/" (:_id p))} (:name p)]])))
+      [:div
+       [:h2 [:a {:href (str "/producers/" (:_id p))} (:name p)]]
+       [:p (-> p :description cut-description)]
+       ]
+      )))
 
 (defn producer [p]
   (template
@@ -47,3 +67,22 @@
       (anti-forgery-field)
 
       (form/submit-button "Sačuvaj izmene"))))
+
+(defn admin-login [& [message]]
+  (template
+    (when message
+      [:div.alert.alert-danger message])
+    (form/form-to
+      [:post "/admin/login"]
+
+      [:div.form-group
+       (form/label "username" "Email adresa")
+       (form/text-field  {:class "form-control"} "username")]
+
+      [:div.form-group
+       (form/label "password" "Lozinka")
+       (form/password-field {:class "form-control"} "password")
+       [:br]]
+
+      (anti-forgery-field)
+      (form/submit-button {:class "btn btn-primary"} "Uloguj se"))))
